0c17fde62230c45d438f79a8aaef683d20ce6f62
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2020-01-11 15:59:51 -0800

    add prompt

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 int main()
 {
       std::string input;
+
+      std::cout << "Enter equation: " << std::endl;
+
       std::getline(std::cin, input);
       std::cout << std::endl;
 
       Equation x(input);
-
       std::cout << "Answer: " << x.calculate() << std::endl;
 
 
